7d8c540c728f44bed029f6ef7695f2fd0bbd8f52
Tiago Rodrigues <tiagopbr.2001@gmail.com>
2022-02-22 20:15:23 +0000

    Indent package

--- .doom.d/packages.el
+++ .doom.d/packages.el
@@ -18,7 +18,8 @@
 (package! editorconfig)
 (package! prettier-js
   :recipe (:host github :repo "prettier/prettier-emacs"))
-(package! lsp-tailwindcss :recipe (:host github :repo "merrickluo/lsp-tailwindcss") )
+(package! lsp-tailwindcss
+  :recipe (:host github :repo "merrickluo/lsp-tailwindcss") )
 ;; To install a package directly from a remote git repo, you must specify a
 ;; `:recipe'. You'll find documentation on what `:recipe' accepts here:
 ;; https://github.com/raxod502/straight.el#the-recipe-format
