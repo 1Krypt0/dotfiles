868dff4b5415dd44af21ee53330ece0056919d9a
Tiago Rodrigues <tiagopbr.2001@gmail.com>
2022-03-14 14:10:14 +0000

    Add new bloat to Emacs

--- .doom.d/packages.el
+++ .doom.d/packages.el
@@ -20,6 +20,9 @@
   :recipe (:host github :repo "prettier/prettier-emacs"))
 (package! lsp-tailwindcss
   :recipe (:host github :repo "merrickluo/lsp-tailwindcss") )
+(package! catppucin-theme
+  :recipe (:host github :repo "pspiagicw/catppuccin-emacs"
+           :files ("catppuccin-theme.el")))
 ;; To install a package directly from a remote git repo, you must specify a
 ;; `:recipe'. You'll find documentation on what `:recipe' accepts here:
 ;; https://github.com/raxod502/straight.el#the-recipe-format
