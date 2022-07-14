77f71adcb19ec1f9ef09e66e9f4399b0572005fe
Tiago Rodrigues <tiagopbr.2001@gmail.com>
2022-07-14 12:02:18 +0100

    Rebind some keys. Remove email from emacs. Fix polybar errors

--- .config/polybar/scripts/modules/get_spotify_status.sh
+++ .config/polybar/scripts/modules/get_spotify_status.sh
@@ -39,12 +39,10 @@ else
     if [ "$STATUS" = "Stopped" ]; then
         echo "No music is playing"
     elif [ "$STATUS" = "Paused"  ]; then
-        update_hooks "$PARENT_BAR_PID" 2
         playerctl --player=$PLAYER metadata --format "$FORMAT"
     elif [ "$STATUS" = "No player is running"  ]; then
         echo "$STATUS"
     else
-        update_hooks "$PARENT_BAR_PID" 1
         playerctl --player=$PLAYER metadata --format "$FORMAT"
     fi
 fi
