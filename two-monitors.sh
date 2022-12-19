f39513b99fe320c6d4837fe52dd579615c8d9944
Tiago Rodrigues <tiagopbr.2001@gmail.com>
2022-12-19 10:12:33 +0000

    update graphics settings on two monitor setup

--- two-monitors.sh
+++ two-monitors.sh
@@ -1,14 +1,15 @@
 #!/usr/bin/env bash
 
-xrandr --output eDP-1 --off
+xrandr --output eDP-1-1 --off &
 
-xrandr --output HDMI-1-0 --primary --mode 1920x1080 --rotate normal --output DP-1-1 --mode 1920x1080 --rotate normal --right-of HDMI-1-0
+xrandr --output HDMI-0 --primary --mode 1920x1080 --rotate normal --output DP-1 --mode 1920x1080 --rotate normal --right-of HDMI-0 &
 
-bspc monitor DP-1-1 -d web brain discord reader spotify
-bspc monitor HDMI-1-0 -d term dev code
+bspc monitor eDP-1-1 --remove &
+bspc monitor DP-1 -d web brain discord reader spotify &
+bspc monitor HDMI-0 -d term dev code &
 
-killall polybar
+killall polybar &
 
-"$HOME"/.config/polybar/two-monitors.sh
+"$HOME"/.config/polybar/two-monitors.sh &
 
-setxkbmap pt &
+setxkbmap us dvp &
