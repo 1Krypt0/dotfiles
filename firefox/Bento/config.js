a2e1c11449fd86c982a36951cac4574d07741355
Tiago Rodrigues <tiagopbr.2001@gmail.com>
2023-09-15 13:09:00 +0100

    Update file modes

--- firefox/Bento/config.js
+++ firefox/Bento/config.js
@@ -11,23 +11,22 @@ const CONFIG = {
   // └─┘┴ ┴└─┘┴└─┘└─┘
 
   // General
-  name: 'Tiago',
+  name: "Tiago",
   imageBackground: false,
   openInNewTab: false,
 
   // Greetings
-  greetingMorning: 'Good morning,',
-  greetingAfternoon: 'Good afternoon,',
-  greetingEvening: 'Good evening,',
-  greetingNight: 'Go to Sleep,',
+  greetingMorning: "Good morning,",
+  greetingAfternoon: "Good afternoon,",
+  greetingEvening: "Good evening,",
+  greetingNight: "Go to Sleep,",
 
   // Weather
-  weatherKey: '516ad24f2ba564b4bcdb9c66129d254f',
-  weatherIcons: 'OneDark', // 'Nord', 'Dark', 'White'
-  weatherUnit: 'C',
-  weatherLatitude: '41.532051',
-  weatherLongitude: '-8.619053',
-
+  weatherKey: "516ad24f2ba564b4bcdb9c66129d254f",
+  weatherIcons: "OneDark", // 'Nord', 'Dark', 'White'
+  weatherUnit: "C",
+  weatherLatitude: "41.532051",
+  weatherLongitude: "-8.619053",
 
   // ┌─┐┌─┐┬─┐┌┬┐┌─┐
   // │  ├─┤├┬┘ ││└─┐
@@ -36,40 +35,40 @@ const CONFIG = {
   // Links
   cards: [
     {
-      id: '1',
-      name: 'Github',
-      icon: 'github',
-      link: 'https://github.com/1Krypt0',
+      id: "1",
+      name: "Github",
+      icon: "github",
+      link: "https://github.com/1Krypt0",
     },
     {
-      id: '2',
-      name: 'Mail',
-      icon: 'mail',
-      link: 'https://mail.google.com/',
+      id: "2",
+      name: "Mail",
+      icon: "mail",
+      link: "https://mail.google.com/",
     },
     {
-      id: '3',
-      name: 'Todoist',
-      icon: 'trello',
-      link: 'https://todoist.com/app/today#',
+      id: "3",
+      name: "Routine",
+      icon: "trello",
+      link: "https://app.routine.co/",
     },
     {
-      id: '4',
-      name: 'Calendar',
-      icon: 'calendar',
-      link: 'https://calendar.google.com/calendar/r',
+      id: "4",
+      name: "Calendar",
+      icon: "calendar",
+      link: "https://calendar.google.com/calendar/r",
     },
     {
-      id: '5',
-      name: 'Reddit',
-      icon: 'bookmark',
-      link: 'https://reddit.com',
+      id: "5",
+      name: "Reddit",
+      icon: "bookmark",
+      link: "https://reddit.com",
     },
     {
-      id: '6',
-      name: 'Youtube',
-      icon: 'youtube',
-      link: 'https://youtube.com/',
+      id: "6",
+      name: "Youtube",
+      icon: "youtube",
+      link: "https://youtube.com/",
     },
   ],
 
@@ -78,95 +77,99 @@ const CONFIG = {
   // ┴─┘┴└─┘ ┴ └─┘
 
   //Icons
-  firstListIcon: 'book-open',
-  secondListIcon: 'briefcase',
-  thirdListIcon: 'zap',
-  fourthListIcon: 'link',
+  firstListIcon: "book-open",
+  secondListIcon: "briefcase",
+  thirdListIcon: "zap",
+  fourthListIcon: "link",
 
   // Links
   lists: {
     firstList: [
       {
-        name: 'College Info Geek',
-        link: 'https://collegeinfogeek.com',
+        name: "College Info Geek",
+        link: "https://collegeinfogeek.com",
       },
       {
-        name: 'Art of Manliness',
-        link: 'https://www.artofmanliness.com/',
+        name: "Art of Manliness",
+        link: "https://www.artofmanliness.com/",
       },
       {
-        name: 'Netflix',
-        link: 'https://www3.watchserieshd.ru/',
+        name: "Netflix",
+        link: "https://www3.watchserieshd.ru/",
       },
       {
-        name: 'All the Books',
-        link: 'https://z-lib.org/',
+        name: "Omnivore",
+        link: "https://www.omnivore.app/",
       },
       {
-        name: 'Omnivore',
-        link: 'https://www.omnivore.app/',
+        name: "Study Music",
+        link: "https://www.youtube.com/c/StudyMD/videos",
       },
     ],
     secondList: [
       {
-        name: 'Webmail',
-        link: 'https://webmail.edu.up.pt/',
+        name: "Webmail",
+        link: "https://webmail.edu.up.pt/",
       },
       {
-        name: 'FEUP',
-        link: 'https://sigarra.up.pt/feup/pt/fest_geral.cursos_list?pv_num_unico=201907021',
+        name: "FEUP",
+        link: "https://sigarra.up.pt/feup/pt/fest_geral.cursos_list?pv_num_unico=201907021",
       },
       {
-        name: 'Moodle',
-        link: 'https://moodle.up.pt/',
+        name: "Moodle",
+        link: "https://moodle.up.pt/",
       },
       {
-        name: 'Study Music',
-        link: 'https://www.youtube.com/c/StudyMD/videos',
+        name: "Canvas",
+        link: "https://canvas.utwente.nl/",
+      },
+      {
+        name: "Rooster",
+        link: "https://rooster.utwente.nl/schedule",
       },
     ],
     thirdList: [
       {
-        name: 'Ape to Gentleman',
-        link: 'https://www.apetogentleman.com/',
+        name: "Ape to Gentleman",
+        link: "https://www.apetogentleman.com/",
       },
       {
-        name: 'bodyweightfitness',
-        link: 'https://www.reddit.com/r/bodyweightfitness/',
+        name: "bodyweightfitness",
+        link: "https://www.reddit.com/r/bodyweightfitness/",
       },
       {
-        name: 'Fitness Wiki',
-        link: 'https://thefitness.wiki/',
+        name: "Fitness Wiki",
+        link: "https://thefitness.wiki/",
       },
       {
-        name: 'Budget Bytes',
-        link: 'https://www.budgetbytes.com/',
+        name: "Budget Bytes",
+        link: "https://www.budgetbytes.com/",
       },
     ],
     fourthList: [
       {
-        name: 'FSI - Monday',
-        link: 'https://videoconf-colibri.zoom.us/j/82659385429',
+        name: "FSI - Monday",
+        link: "https://videoconf-colibri.zoom.us/j/82659385429",
       },
       {
-        name: 'FSI - Thursday',
-        link: 'https://videoconf-colibri.zoom.us/j/81203543687',
+        name: "FSI - Thursday",
+        link: "https://videoconf-colibri.zoom.us/j/81203543687",
       },
       {
-        name: 'LBAW',
-        link: 'https://videoconf-colibri.zoom.us/j/81240547359',
+        name: "LBAW",
+        link: "https://videoconf-colibri.zoom.us/j/81240547359",
       },
       {
-        name: 'LTW',
-        link: 'https://fc-up-pt.zoom.us/j/81509620746'
+        name: "LTW",
+        link: "https://fc-up-pt.zoom.us/j/81509620746",
       },
       {
-        name: 'PFL',
-        link: 'https://videoconf-colibri.zoom.us/j/87911714265?pwd=NFRpUkJHZzBpVENLWTFUZGxwVGNXUT09',
+        name: "PFL",
+        link: "https://videoconf-colibri.zoom.us/j/87911714265?pwd=NFRpUkJHZzBpVENLWTFUZGxwVGNXUT09",
       },
       {
-        name: 'RC',
-        link: 'https://videoconf-colibri.zoom.us/j/98394920212?pwd=MTBNREo3d2J0Yk9YcXBSMTl3SWttUT09',
+        name: "RC",
+        link: "https://videoconf-colibri.zoom.us/j/98394920212?pwd=MTBNREo3d2J0Yk9YcXBSMTl3SWttUT09",
       },
     ],
   },
